2d4d10562d40f7c4fd991e78405d2656d819b262
Henry Egwanumku <egwa1@outlook.com>
2023-12-02 19:59:37 +0100

    fit the contact with the template

--- assets/js/contacts_js.js
+++ assets/js/contacts_js.js
@@ -5,47 +5,40 @@ let users = [
     telefon: +491514567813,
   },
   {
-    id: 2,
     name: "Anita Gant",
     email: "anita@aol.com",
     telefon: +491510527872,
   },
   {
-    id: 3,
     name: "Elena James",
     email: "elenajames@gmail.com",
     telefon: +497714567894,
   },
   {
-    id: 4,
     name: "Zoe Graber",
     email: "zoegraber@web.de",
     telefon: +4915145678512,
   },
   {
-    id: 5,
     name: "Shawn Peter",
     email: "sp@live.com",
     telefon: +491602135121,
   },
   {
-    id: 6,
     name: "Seun Dede",
     email: "seun@outlook.com",
     telefon: +491554567890,
   },
   {
-    id: 7,
     name: "Christina Justus",
     email: "cjustus@gmail.com",
     telefon: +491781161324,
   },
-  // {
-  //   id: 8,
-  //   name: "Philip Chanel",
-  //   email: "philipchanel@yahoo.com",
-  //   telefon: +491514567890,
-  // },
+  {
+    name: "Philip Chanel",
+    email: "philipchanel@yahoo.com",
+    telefon: +491514567890,
+  },
 ];
 
 window.onload = contacts_init();
@@ -53,33 +46,35 @@ window.onload = contacts_init();
 const userName = document.getElementById("name_email");
 const userInfo = document.getElementById("contact_details");
 const clickedContact = document.querySelector(".contact_name");
-
+let initials;
 let currentUserIndex;
 let currentAlphabet = "";
 
 users.sort((a, b) => a.name.localeCompare(b.name));
 
 async function includeHTML() {
-  let includeElements = document.querySelectorAll('[w3-include-html]'); //Alle Elemente mit w3-include-html in einem Array Speichern
-  for (let i = 0; i < includeElements.length; i++) { //Durch das ganze array durchgehen
-      const element = includeElements[i];
-      file = element.getAttribute("w3-include-html"); // "includes/header.html" Speichert den dateinpfad
-      let resp = await fetch(file); //Anfrage an die file zum laden. Anwort in resp speichern
-      if (resp.ok) {
-          element.innerHTML = await resp.text(); // Der ganze string aus header.html ist dadrinne gespeichert.
-      } else {
-          element.innerHTML = 'Page not found';
-      }
+  let includeElements = document.querySelectorAll("[w3-include-html]"); //Alle Elemente mit w3-include-html in einem Array Speichern
+  for (let i = 0; i < includeElements.length; i++) {
+    //Durch das ganze array durchgehen
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html" Speichert den dateinpfad
+    let resp = await fetch(file); //Anfrage an die file zum laden. Anwort in resp speichern
+    if (resp.ok) {
+      element.innerHTML = await resp.text(); // Der ganze string aus header.html ist dadrinne gespeichert.
+    } else {
+      element.innerHTML = "Page not found";
+    }
   }
 }
 
-async function contacts_init(){
-    await includeHTML();
-    menue_clicked('contacts');
-    displayUsers();
+async function contacts_init() {
+  await includeHTML();
+  menue_clicked("contacts");
+  displayUsers();
 }
 
 function displayUsers() {
+  users.sort((a, b) => a.name.localeCompare(b.name));
   userName.innerHTML = "";
   for (let i = 0; i < users.length; i++) {
     const user = users[i];
@@ -106,16 +101,16 @@ function displayUserDetails(i) {
   let nameInitials = names[0].charAt(0).toUpperCase();
   nameInitials += names[names.length - 1].charAt(0).toUpperCase();
   userInfo.innerHTML += userDetailHTML(i, nameInitials);
-  userInfo.classList.remove("translate");
-  setTimeout(() => {
-    userInfo.classList.add("translate");
-  }, 500);
 }
 
-function deleteUser(userTelephone, i) {
-  users = users.filter((user) => user.telefon !== userTelephone);
-  displayUsers();
-  displayUserDetails(i);
+function deleteUserByIndex(index) {
+  currentUserIndex = index;
+  if (currentUserIndex >= 0 && currentUserIndex < users.length) {
+    users.splice(currentUserIndex, 1);
+    displayUsers();
+    displayUserDetails();
+    closeEditContactPopup();
+  }
 }
 
 function openAddContactPopup() {
@@ -131,13 +126,15 @@ function addContactPopup() {
   if (!newName || !newEmail || !newTelefon) {
     return;
   }
-  users.push({ name: newName, email: newEmail, telefon: newTelefon });
+  const newUser = { name: newName, email: newEmail, telefon: newTelefon };
+  users.push(newUser);
   users.sort((a, b) => a.name.localeCompare(b.name));
-
+  const newUserIndex = users.findIndex((user) => user === newUser);
+  currentUserIndex = newUserIndex;
   clearForm();
   closeAddContactPopup();
   displayUsers();
-  displayUserDetails();
+  displayUserDetails(currentUserIndex);
 }
 
 function clearForm() {
@@ -153,12 +150,12 @@ function closeAddContactPopup() {
   document.getElementById("overlay").style.display = "none";
 }
 
-function openEditContactPopup(index) {
-  currentUserIndex = index;
+function openEditContactPopup(currentUserIndex) {
   const user = users[currentUserIndex];
   document.getElementById("editName").value = user.name;
   document.getElementById("editEmail").value = user.email;
   document.getElementById("editPhone").value = user.telefon;
+  contactInitials.innerText = initials;
   document.getElementById("editContactPopup").classList.add("show_add_contact");
   document.getElementById("overlay").style.display = "block";
 }
@@ -171,10 +168,9 @@ function saveChanges() {
   user.name = editedName;
   user.email = editedEmail;
   user.telefon = editedPhone;
-  users.sort((a, b) => a.name.localeCompare(b.name));
   closeEditContactPopup();
-  displayUsers();
   displayUserDetails(currentUserIndex);
+  displayUsers();
 }
 
 function closeEditContactPopup() {
@@ -187,59 +183,60 @@ function closeEditContactPopup() {
 function userHTML(i, firstAlphabet, nameInitials) {
   const user = users[i];
   return `
-    <div class="alphabet" id="alphabet">
-    <h3>${firstAlphabet}</h3>
-  </div>
-  <div class="contacts_initials">
-    <a href="#" class="contact_name anton" onclick="displayUserDetails(${i})">
+      <div class="alphabet" id="alphabet">
+      <h3>${firstAlphabet}</h3>
+    </div>
+    <div class="contacts_initials">
+      <a href="#" class="contact_name anton" onclick="displayUserDetails(${i})">
+        <span>${nameInitials}</span>
+        <div>
+          <h4>${user.name}</h4>
+          <h5>${user.email}</h5>
+        </div>
+      </a>
+    </div>
+        `;
+}
+
+function sortedUserHTML(i, nameInitials) {
+  const user = users[i];
+  return ` 
+      <a href="#" class="contact_name anton" onclick="displayUserDetails(${i})">
       <span>${nameInitials}</span>
       <div>
         <h4>${user.name}</h4>
         <h5>${user.email}</h5>
       </div>
     </a>
-  </div>
       `;
-}
-
-function sortedUserHTML(i, nameInitials) {
-  const user = users[i];
-  return ` 
-    <a href="#" class="contact_name anton" onclick="displayUserDetails(${i})">
-    <span>${nameInitials}</span>
-    <div>
-      <h4>${user.name}</h4>
-      <h5>${user.email}</h5>
-    </div>
-  </a>
-    `;
 }
 
 function userDetailHTML(i, nameInitials) {
   currentUserIndex = i;
+  initials = nameInitials;
   let user = users[currentUserIndex];
   return `
-  <div class="info">
-  <div class="contacts_edit" id="username_info">
-    <p>${nameInitials}</p>
-    <div class="contactname">
-      <h3>${user.name}</h3>
-      <div class="contact_icon">
-        <span onclick="openEditContactPopup(${i})"  class="delete_edit"><i class="fa-solid fa-pencil"></i><span>Edit</span></span>
-        <span class="delete_edit"  onclick="deleteUser(${user.telefon})"
-          ><i class="fa-regular fa-trash-can"></i
-          ><span>Delete</span></span
-        >
+    <div class="info">
+    <div class="contacts_edit" id="username_info">
+      <p>${initials}</p>
+      <div class="contactname">
+        <h3>${user.name}</h3>
+        <div class="contact_icon">
+          <span onclick="openEditContactPopup(${i})"  class="delete_edit"><i class="fa-solid fa-pencil"></i><span>Edit</span></span>
+          <span class="delete_edit"  onclick="deleteUserByIndex(${i})"
+            ><i class="fa-regular fa-trash-can"></i
+            ><span>Delete</span></span
+          >
+        </div>
       </div>
     </div>
-  </div>
-  <div class="contact_information">
-    <h4>Contact Information</h4>
-    <h5>Email</h5>
-    <h5 class="email">${user.email}</h5>
-    <h5>Phone</h5>
-    <h5>${user.telefon}</h5>
-  </div>
-  </div>
-  `;
+    <div class="contact_information">
+      <h4>Contact Information</h4>
+      <h5>Email</h5>
+      <h5 class="email">${user.email}</h5>
+      <h5>Phone</h5>
+      <h5>${user.telefon}</h5>
+    </div>
+    </div>
+    `;
 }
