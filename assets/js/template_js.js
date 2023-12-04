4579d50af899c9c5dcd4615e711bdf451b867cf5
Jan Karras <karras.jan@web.de>
2023-12-04 17:46:26 +0100

    help

--- assets/js/template_js.js
+++ assets/js/template_js.js
@@ -11,3 +11,18 @@ async function menue_clicked(name) {
   }
   document.getElementById(name).classList.add("btn_clicked");
 }
+
+async function includeHTML() {
+  let includeElements = document.querySelectorAll("[w3-include-html]"); //Alle Elemente mit w3-include-html in einem Array Speichern
+  for (let i = 0; i < includeElements.length; i++) {
+    //Durch das ganze array durchgehen
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html" Speichert den dateinpfad
+    let resp = await fetch(file); //Anfrage an die file zum laden. Anwort in resp speichern
+    if (resp.ok) {
+      element.innerHTML = await resp.text(); // Der ganze string aus header.html ist dadrinne gespeichert.
+    } else {
+      element.innerHTML = "Page not found";
+    }
+  }
+}
