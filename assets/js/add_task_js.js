ecc0c07af05449a378f4099ea7b52318a8112ba7
Henry Egwanumku <egwa1@outlook.com>
2023-12-06 19:57:18 +0100

    adjustments

--- assets/js/add_task_js.js
+++ assets/js/add_task_js.js
@@ -1,2 +1,36 @@
-async function add_task_init(){
+async function add_task_init() {
+  contacts();
+}
+
+function toggleOptions() {
+  const optionsContainer = document.getElementById("optionsContainer");
+  optionsContainer.style.display =
+    optionsContainer.style.display === "block" ? "none" : "block";
+}
+
+function selectUser(index) {
+  const selectedOption = document.getElementById("selectedOption");
+  selectedOption.textContent = users[index].name;
+  toggleOptions();
+}
+
+function contacts() {
+  const optionsContainer = document.getElementById("optionsContainer");
+  optionsContainer.innerHTML = "";
+  for (let i = 0; i < users.length; i++) {
+    optionsContainer.innerHTML += `<div class="option" onclick="selectUser(${i})">${users[i].name}</div>`;
+  }
+}
+function selectUser(index) {
+  const selectedOption = document.getElementById("selectedOption");
+  selectedOption.textContent = users[index].name;
+  toggleOptions();
+}
+
+function contacts() {
+  const optionsContainer = document.getElementById("optionsContainer");
+  optionsContainer.innerHTML = "";
+  for (let i = 0; i < users.length; i++) {
+    optionsContainer.innerHTML += `<div class="option" onclick="selectUser(${i})">${users[i].name}</div>`;
+  }
 }
