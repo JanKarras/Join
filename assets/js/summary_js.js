bae91f160f42a8d28b052084abfe9483d462cf54
Jan Karras <karras.jan@web.de>
2023-12-11 18:52:17 +0100

    backend tasks

--- assets/js/summary_js.js
+++ assets/js/summary_js.js
@@ -4,7 +4,7 @@
  * Calls 'load_numbers' to load relevant numbers or data on the page.
  * Calls 'updateTime' to update the greeting message based on the current time.
  */
-async function summary_init(){
+async function summary_init() {
     await get_all_user();
     set_name();
     load_numbers();
@@ -16,14 +16,13 @@ async function summary_init(){
  * Retrieves the user's name from the 'all_user' array based on the logged-in email.
  * Displays a greeting message with the full name and a shortened version with initials.
  */
-function set_name(){
+function set_name() {
     let name;
     for (let i = 0; i < all_user.length; i++) {
         const element = all_user[i];
-        if (element['email'] == Email)
-        {
+        if (element['email'] == Email) {
             name = element['name']
-            break ;
+            break;
         }
     }
     document.getElementById('greeting_name').innerHTML = name;
@@ -32,49 +31,50 @@ function set_name(){
     let second_name = "";
     if (name_parts.length > 1) {
         second_name = name_parts[name_parts.length - 1].charAt(0).toUpperCase();
-      }
+    }
     document.getElementById('name').innerHTML = firt_name + second_name;
 }
 
 /**
  * Lädt die Anzahl der Aufgaben für den Benutzer mit der angegebenen E-Mail-Adresse und rendert die Zahlen.
  */
-function load_numbers(){
+function load_numbers() {
     for (let i = 0; i < all_user.length; i++) {
         const element = all_user[i];
-        if (element['email'] == Email)
-        {
+        if (element['email'] == Email) {
             let tasks = element['tasks'];
             render_numbers(tasks);
-            break ;
+            break;
         }
-    }  
+    }
 }
 
 /**
  * Rendert die Anzahl der Aufgaben in verschiedenen Kategorien und Informationen zu dringenden Aufgaben.
  * @param {Array} tasks - Das Array der Aufgaben des Benutzers.
  */
-function render_numbers(tasks){
-    document.getElementById('to_do_nb').innerHTML = tasks[0]['to_do'].length;
-    document.getElementById('done_nb').innerHTML = tasks[0]['done'].length;
-    document.getElementById('board_nb').innerHTML = tasks.length;
-    document.getElementById('progrss_nb').innerHTML = tasks[0]['in_progress'].length;
-    document.getElementById('feedback_nb').innerHTML = tasks[0]['feedback'].length;
-    let count = 0;
-    for (let i = 0; i < tasks.length; i++) {
-        const element = tasks[i];
-        if (element['done'][0]['prio'] == "urgent")
-            count++;
-        if (element['to_do'][0]['prio'] == "urgent")
-            count++;
-        if (element['in_progress'][0]['prio'] == "urgent")
-            count++;
-        if (element['feedback'][0]['prio'] == "urgent")
-            count++;
+function render_numbers(tasks) {
+    if (tasks.length != 0) {
+        document.getElementById('to_do_nb').innerHTML = tasks[0]['to_do'].length;
+        document.getElementById('done_nb').innerHTML = tasks[0]['done'].length;
+        document.getElementById('board_nb').innerHTML = tasks.length;
+        document.getElementById('progrss_nb').innerHTML = tasks[0]['in_progress'].length;
+        document.getElementById('feedback_nb').innerHTML = tasks[0]['feedback'].length;
+        let count = 0;
+        for (let i = 0; i < tasks.length; i++) {
+            const element = tasks[i];
+            if (element['done'][0]['prio'] == "urgent")
+                count++;
+            if (element['to_do'][0]['prio'] == "urgent")
+                count++;
+            if (element['in_progress'][0]['prio'] == "urgent")
+                count++;
+            if (element['feedback'][0]['prio'] == "urgent")
+                count++;
+        }
+        document.getElementById('urgent_nb').innerHTML = count;
+        render_deadline(tasks);
     }
-    document.getElementById('urgent_nb').innerHTML = count;
-    render_deadline(tasks);
 }
 
 /**
@@ -116,14 +116,14 @@ function render_deadline(tasks) {
  * Updates the greeting message based on the current time of day.
  * Retrieves the current hour and displays an appropriate greeting message.
  */
-function updateTime(){
+function updateTime() {
     let currentTime = new Date();
     let currenthour = currentTime.getHours();
-    if (currenthour < 12){
+    if (currenthour < 12) {
         document.getElementById('greeting').innerHTML = "Good morning";
-    } else if (currenthour > 12 && currenthour < 14){
+    } else if (currenthour > 12 && currenthour < 14) {
         document.getElementById('greeting').innerHTML = "Good noon";
-    } else if (currenthour > 14 && currenthour < 18){
+    } else if (currenthour > 14 && currenthour < 18) {
         document.getElementById('greeting').innerHTML = "Good afternoon";
     } else if (currenthour > 18)
         document.getElementById('greeting').innerHTML = "Good evening";
