1e910dab5fa22c574309c16383254c62a43773dd
Henry Egwanumku <egwa1@outlook.com>
2023-12-12 04:39:54 +0100

    added addtask popup to board

--- assets/js/board_js.js
+++ assets/js/board_js.js
@@ -1,3 +1,16 @@
 async function board_init() {
   contacts();
 }
+
+function openAddTask() {
+  let addTask = document.getElementById("addTaskPopUpWindowContent");
+  let overlay = document.querySelector(".overlay");
+  addTask.classList.remove("d-none");
+  overlay.classList.remove("d-none");
+}
+function closeAddTask() {
+  let addTask = document.getElementById("addTaskPopUpWindowContent");
+  let overlay = document.querySelector(".overlay");
+  addTask.classList.add("d-none");
+  overlay.classList.add("d-none");
+}
